630976f0023dc993076cc16a0549429041fac008
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-20 18:39:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,14 @@
 class Dog 
-  lassie = Dog.new 
-  lassie.name = "Lassie"
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end 
   
-  lassie.name
-  
+  def name 
+    @this_dogs_name
+  end
 end 
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+puts lassie.name 
